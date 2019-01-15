81ed2d70b95a71460f46bbae52b419b52a1509b3
Ahmed89TN <ahmedbenothmen045@gmail.com>
2019-01-14 18:59:55 -0800

    setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double-Cheese Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Burger", false);
